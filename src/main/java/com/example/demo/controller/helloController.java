0dbed2e2bb062142fcb3d3094fe84b69e771ae1a
nayarapatente <nayara@educacaoadistancia.org.br>
2024-09-16 11:15:56 -0300

    :tada: iniciando Projeto

---
+++ src/main/java/com/example/demo/controller/helloController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+@RequiredArgsConstructor
+public class helloController {
+
+    @GetMapping()
+    public String mostraHello(){
+        return "Hello Word";
+    }
+}
